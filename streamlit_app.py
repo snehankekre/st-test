4535a40c6a9164f804d434d9335213239fc9162f
Johannes Rieke <johannes.rieke@gmail.com>
2022-01-05 22:47:36 +0100

    exception

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,9 @@ from rich.console import Console
 
 console = Console(force_terminal=True, color_system="truecolor")
 
-console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
+try:
+    a = {}
+    a["key"]
+except KeyError as e:
+    console.print_exception()
+    #console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
