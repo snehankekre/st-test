2780cb4b4a5140c355856ca31fa0100abb533246
Johannes Rieke <johannes.rieke@gmail.com>
2022-01-07 00:59:16 +0100

    Add logs

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,13 @@ from rich.console import Console
 
 console = Console(force_terminal=True)
 
-try:
-    a = {}
-    a["key"]
-except KeyError as e:
-    console.print_exception()
-    #console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
+# try:
+#     a = {}
+#     a["key"]
+# except KeyError as e:
+#     console.print_exception()
+#     #console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
+
+console.print("[09:26:03] 🚥 Connecting...")
+console.print("[yellow][09:26:03][/yellow] 🚥 Connecting...")
+st.write("Hello")
