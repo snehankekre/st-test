d63d7d26bf080335060e397d45afc9786f6dfe8e
Johannes Rieke <johannes.rieke@gmail.com>
2022-01-07 01:18:42 +0100

    Add more logs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from rich.console import Console
+from rich.panel import Panel
+from rich.box import HORIZONTALS
 
 console = Console(force_terminal=True)
 
@@ -10,7 +12,6 @@ console = Console(force_terminal=True)
 #     console.print_exception()
 #     #console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
 
-console.print("[yellow][09:26:03][/yellow] 🚥 Connecting...")
 console.print(
     """
     
@@ -28,4 +29,20 @@ console.print(
               - file:  [cyan]/app/app-frontpage/Pipfile[/cyan]
     """
 )
+console.print(Panel("""Installing dependencies from Pipfile.lock (49795d)...
+Ignoring appnope: markers 'sys_platform == "darwin" and platform_system == "Darwin"' don't match your environment
+Collecting backports.zoneinfo==0.2.1
+  Downloading backports.zoneinfo-0.2.1-cp38-cp38-manylinux1_x86_64.whl (74 kB)
+Installing collected packages: backports.zoneinfo
+WARNING: Ignoring invalid distribution -itpython (/home/appuser/venv/lib/python3.8/site-packages)
+WARNING: Ignoring invalid distribution -harset-normalizer (/home/appuser/venv/lib/python3.8/site-packages)
+WARNING: Ignoring invalid distribution -ebugpy (/home/appuser/venv/lib/python3.8/site-packages)
+Successfully installed backports.zoneinfo-0.2.1
+Collecting cachetools==4.2.4
+  Downloading cachetools-4.2.4-py3-none-any.whl (10 kB)
+Installing collected packages: cachetools
+  Attempting uninstall: cachetools
+    Found existing installation: cachetools 4.2.2
+    Uninstalling cachetools-4.2.2:
+      Successfully uninstalled cachetools-4.2.2""", title="pipenv", box=HORIZONTALS, border_style="cyan"))
 st.write("Hello")
