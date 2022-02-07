81ad9bdfede781c4a1a7b99eafda8ee64215bee3
Snehan Kekre <snehan.minerva@gmail.com>
2022-02-07 13:37:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-# from rich.console import Console
-# from rich.panel import Panel
-# from rich.box import Box, HORIZONTALS
+from rich.console import Console
+from rich.panel import Panel
+from rich.box import Box, HORIZONTALS
 import psutil
 import math
 import os
@@ -65,31 +65,31 @@ st.info(
 
 console = Console(force_terminal=True, color_system="truecolor")
 
-# try:
-#     a = {}
-#     a["key"]
-# except KeyError as e:
-#     console.print_exception()
-#     #console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
+try:
+    a = {}
+    a["key"]
+except KeyError as e:
+    console.print_exception()
+    #console.print("Where there is a [bold cyan]Will[/bold cyan] there [u]is[/u] a [i]way[/i].")
 
-# console.print(
-#     """
+console.print(
+    """
     
-# [#f9ca2a][09:26:03][/#f9ca2a] 🚥 Connecting...
-# [#f9ca2a][09:26:04][/#f9ca2a] 🚀 Starting up...
-#               - owner:        [#1be8c5]streamlit[/#1be8c5]
-#               - repository:   [#1be8c5]app-frontpage[/#1be8c5]
-#               - branch:       [#1be8c5]main[/#1be8c5]
-#               - main module:  [#1be8c5]streamlit_app.py[/#1be8c5]
-# [#f9ca2a][09:26:09][/#f9ca2a] 🐙 Cloning repository...
-# [#f9ca2a][09:26:10][/#f9ca2a]    Cloning into [white]'app-frontpage'[/white]...
-# [#f9ca2a][09:26:55][/#f9ca2a]    Cloned repository!                    
-# [#f9ca2a][09:27:00][/#f9ca2a] 📦 Processing dependencies...
-#               - tool:         [#1be8c5]pipenv[/#1be8c5]
-#               - file:         [#1be8c5]/app/app-frontpage/Pipfile[/#1be8c5]
+[#f9ca2a][09:26:03][/#f9ca2a] 🚥 Connecting...
+[#f9ca2a][09:26:04][/#f9ca2a] 🚀 Starting up...
+              - owner:        [#1be8c5]streamlit[/#1be8c5]
+              - repository:   [#1be8c5]app-frontpage[/#1be8c5]
+              - branch:       [#1be8c5]main[/#1be8c5]
+              - main module:  [#1be8c5]streamlit_app.py[/#1be8c5]
+[#f9ca2a][09:26:09][/#f9ca2a] 🐙 Cloning repository...
+[#f9ca2a][09:26:10][/#f9ca2a]    Cloning into [white]'app-frontpage'[/white]...
+[#f9ca2a][09:26:55][/#f9ca2a]    Cloned repository!                    
+[#f9ca2a][09:27:00][/#f9ca2a] 📦 Processing dependencies...
+              - tool:         [#1be8c5]pipenv[/#1be8c5]
+              - file:         [#1be8c5]/app/app-frontpage/Pipfile[/#1be8c5]
 
-#     """
-# )
+    """
+)
 
 CUSTOM_HORIZONTALS = Box(
     """\
@@ -104,26 +104,26 @@ CUSTOM_HORIZONTALS = Box(
 """
 )
 
-# console.print(
-#     Panel(
-#         """Installing dependencies from Pipfile.lock (49795d)...
-# Ignoring appnope: markers 'sys_platform == "darwin" and platform_system == "Darwin"' don't match your environment
-# Collecting backports.zoneinfo==0.2.1
-#   Downloading backports.zoneinfo-0.2.1-cp38-cp38-manylinux1_x86_64.whl (74 kB)
-# Installing collected packages: backports.zoneinfo
-# WARNING: Ignoring invalid distribution -itpython (/home/appuser/venv/lib/python3.8/site-packages)
-# WARNING: Ignoring invalid distribution -harset-normalizer (/home/appuser/venv/lib/python3.8/site-packages)
-# WARNING: Ignoring invalid distribution -ebugpy (/home/appuser/venv/lib/python3.8/site-packages)
-# Successfully installed backports.zoneinfo-0.2.1
-# Collecting cachetools==4.2.4
-#   Downloading cachetools-4.2.4-py3-none-any.whl (10 kB)
-# Installing collected packages: cachetools
-#   Attempting uninstall: cachetools
-#     Found existing installation: cachetools 4.2.2
-#     Uninstalling cachetools-4.2.2:
-#       Successfully uninstalled cachetools-4.2.2""",
-#         title="pipenv",
-#         border_style="#dbbbff",
-#         box=HORIZONTALS,
-#     )
-# )
+console.print(
+    Panel(
+        """Installing dependencies from Pipfile.lock (49795d)...
+Ignoring appnope: markers 'sys_platform == "darwin" and platform_system == "Darwin"' don't match your environment
+Collecting backports.zoneinfo==0.2.1
+  Downloading backports.zoneinfo-0.2.1-cp38-cp38-manylinux1_x86_64.whl (74 kB)
+Installing collected packages: backports.zoneinfo
+WARNING: Ignoring invalid distribution -itpython (/home/appuser/venv/lib/python3.8/site-packages)
+WARNING: Ignoring invalid distribution -harset-normalizer (/home/appuser/venv/lib/python3.8/site-packages)
+WARNING: Ignoring invalid distribution -ebugpy (/home/appuser/venv/lib/python3.8/site-packages)
+Successfully installed backports.zoneinfo-0.2.1
+Collecting cachetools==4.2.4
+  Downloading cachetools-4.2.4-py3-none-any.whl (10 kB)
+Installing collected packages: cachetools
+  Attempting uninstall: cachetools
+    Found existing installation: cachetools 4.2.2
+    Uninstalling cachetools-4.2.2:
+      Successfully uninstalled cachetools-4.2.2""",
+        title="pipenv",
+        border_style="#dbbbff",
+        box=HORIZONTALS,
+    )
+)
